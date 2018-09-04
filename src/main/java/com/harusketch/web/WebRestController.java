1b62531cf1da7ba9c801c4967b2235c8c68a2db1
ryan han <rhep0820@gmail.com>
2018-09-04 14:46:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/harusketch/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.harusketch.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "HelloWorld";
+	}
+}
