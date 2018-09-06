df40fed886b25f7597731d0d367313b934b433c0
ryan han <rhep0820@gmail.com>
2018-09-06 14:53:44 +0900

    test/resources/application.yml 추가

--- src/test/java/com/harusketch/web/WebControllerTest.java
+++ src/test/java/com/harusketch/web/WebControllerTest.java
@@ -25,6 +25,6 @@ public class WebControllerTest {
 		String body = this.restTemplate.getForObject("/", String.class);
 		
 		//then
-		assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+		assertThat(body).contains("하루 스케치");
 	}
 }
