7f2794e45b42ebecc10fcc341c00a1c3fbb4cb9e
ryan han <rhep0820@gmail.com>
2018-09-10 14:56:51 +0900

    Member entity 생성중

---
+++ src/main/java/com/harusketch/domain/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.harusketch.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long>{
+
+}
--- src/main/java/com/harusketch/domain/posts/PostsRepository.java
+++ src/main/java/com/harusketch/domain/posts/PostsRepository.java
@@ -12,8 +12,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface PostsRepository extends JpaRepository<Posts, Long>{
 
 	//@Query를 쓰지 않아도 되지만, SpringDataJPA에서 제공하지 않는 메소드는 @Query로 작성 가능
-	@Query("SELECT p " +
-			"FROM Posts p " +
-			"ORDER BY p.id DESC")
+	@Query("SELECT p FROM Posts p ORDER BY p.id DESC")
 	Stream<Posts> findAllDesc();
 }
