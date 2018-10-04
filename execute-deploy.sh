dbd8b5e1725d02acc34cc4037c8eeca6f64440d0
ryan han <rhep0820@gmail.com>
2018-10-04 17:57:00 +0900

    local의 execute-deploy.sh 수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+## /home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
 ## deploy.sh를 백그라운드로 실행한 뒤, 로그나 기타 내용을 남기지 않도록 처리
+
+/home/ec2-user/app/nonstop/deploy.sh > /dev/null 2> /dev/null < /dev/null &
